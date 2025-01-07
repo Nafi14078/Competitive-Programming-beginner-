2d6c03691ee29e337fa07425dddf2efa2bd2a973
Ashfak Azad Nafi <ashfakazadnafi@gmail.com>
2025-01-07 10:58:18 +0600

    codeforce

--- Codeforce/Petya and Strings.cpp
+++ Codeforce/Petya and Strings.cpp
@@ -7,40 +7,28 @@ int main()
     cin >> s1;
     cin >> s2;
     string n1, n2;
-    int count1 = 0;
-    int count2 = 0;
     for (int i = 0; i < s1.size(); i++)
     {
-        s1[i] = tolower(s1[i]);
-        n1 = n1 + s1[i];
+        n1 = tolower(s1[i]);
+        n2 = tolower(s2[i]);
+        if (n1 == n2)
+        {
+            continue;
+        }
+        else if (n1 > n2)
+        {
+            cout << "1" << endl;
+            break;
+        }
+        else
+        {
+            cout << "-1" << endl;
+            break;
+        }
     }
-    for (int i = 0; i < s2.size(); i++)
-    {
-        s2[i] = tolower(s2[i]);
-        n2 = n2 + s2[i];
-    }
-
-    for (int i = 0; i < n1.size(); i++)
-    {
-        count1 = count1 + int(n1[i]);
-    }
-    for (int i = 0; i < n2.size(); i++)
-    {
-        count2 = count2 + int(n2[i]);
-    }
-
-    if (count1 == count2)
+    if (n1 == n2)
     {
         cout << "0" << endl;
     }
-    else if (count1 > count2)
-    {
-        cout << "1" << endl;
-    }
-    else
-    {
-        cout << "-1" << endl;
-    }
-
     return 0;
 }
