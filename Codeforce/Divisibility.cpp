4c56f0eb228e0126d1cfe29322bca77c723919ec
Ashfak Azad Nafi <ashfakazadnafi@gmail.com>
2025-01-11 16:52:34 +0600

    codeforce

---
+++ Codeforce/Divisibility.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long int a, b;
+        cin >> a >> b;
+        if (a % b != 0)
+        {
+            cout << b - (a % b) << endl;
+        }
+        else
+        {
+            cout << "0" << endl;
+        }
+    }
+    return 0;
+}
